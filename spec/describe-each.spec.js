e85d3cd0db4aa9cb174045ca4de29508c6b86f78
Ian Purvis <ian@purvisresearch.com>
2023-03-11 11:27:28 -0600

    Refactor describe specs using mocks

--- spec/describe-each.spec.js
+++ spec/describe-each.spec.js
@@ -1,177 +1,73 @@
 import { expect, jest } from '@jest/globals'
 import { afterAll, beforeAll, beforeEach, describe, test } from '../src/index.js'
-import { noop } from './helpers.js'
+import { contextFromLoopArgs } from '../src/util.js'
+import { fakeContext, noop } from './helpers.js'
 
-describe('given a table of arrays', () => {
+const subjects = [
+  {
+    name: 'a table of arrays',
+    table:[
+      [ 0, 0, 0 ],
+      [ 0, 1, 1 ],
+      [ 1, 0, 1 ]
+    ]
+  },
+  {
+    name: 'a table of objects',
+    table: [
+      { a: 0, b: 0, c: 0 },
+      { a: 0, b: 1, c: 1 },
+      { a: 1, b: 0, c: 1 }
+    ]
+  },
+  {
+    name: 'a table of values',
+    table: [
+      1,
+      2,
+      3
+    ]
+  }
+]
 
-  const table = [
-    [ 0, 0, 0 ],
-    [ 0, 1, 1 ],
-    [ 1, 0, 1 ]
-  ]
+for (const { name, table } of subjects) {
+  describe(`given ${name}`, () => {
+    const outerBeforeAllFn = jest.fn(() => fakeContext())
+    const innerBeforeAllFn = jest.fn(() => fakeContext())
+    const innerTestFn = jest.fn()
+    const describeFn = jest.fn(() => {
+      beforeAll(innerBeforeAllFn)
+      test('mock test', innerTestFn)
+    })
 
-  let i, groupContextBefore
+    beforeAll(outerBeforeAllFn)
+    describe.each(table)('mock describe %#', describeFn)
 
-  beforeAll((context) => {
-    i = 0
-    groupContextBefore = context
-  })
-
-  describe.each(table)('iteration %#', (...row) => {
-
-    let groupContext, loopContext, testContext
-
-    beforeAll((context) => {
-      groupContext = context
-      loopContext = {
-        '0': row[0],
-        '1': row[1],
-        '2': row[2]
+    test('creates a group for each row of the table', () => {
+      for (let i = 0, row, expected; i < table.length; i++) {
+        row = table[i]
+        expected = Array.isArray(row) ? row : [row]
+        expect(describeFn).toHaveBeenNthCalledWith(i+1, ...expected)
       }
+      expect(describeFn).toHaveBeenCalledTimes(table.length)
     })
 
-    beforeEach((context) => {
-      testContext = context
-    })
-
-    test('calls the hook with a row from the table', () => {
-      expect(row).toEqual(table[i])
-    })
-
-    test('pushes a copy of the parent scope onto the stack before all', () => {
-      expect(groupContext).toMatchObject(groupContextBefore)
-    })
-
-    test('merges loop context into group context before all', () => {
-      expect(groupContext).toMatchObject(loopContext)
-    })
-
-    test('assigns group context to test context before each', () => {
-      expect(testContext).toBe(groupContext)
-    })
-
-    afterAll(() => { i++ })
-  })
-
-  test('calls the hook for all table rows', () => {
-    expect(i).toEqual(table.length)
-  })
-
-  test('pops the scope off the stack after all', (context) => {
-    expect(context).toBe(groupContextBefore)
-  })
-})
-
-describe('given a table of objects', () => {
-
-  const table = [
-    { a: 0, b: 0, c: 0 },
-    { a: 0, b: 1, c: 1 },
-    { a: 0, b: 2, c: 2 }
-  ]
-
-  let i, groupContextBefore
-
-  beforeAll((context) => {
-    i = 0
-    groupContextBefore = context
-  })
-
-  describe.each(table)('iteration %#', (...row) => {
-
-    let groupContext, loopContext, testContext
-
-    beforeAll((context) => {
-      groupContext = context
-      loopContext = row[0]
-    })
-
-    beforeEach((context) => {
-      testContext = context
-    })
-
-    test('calls the hook with a row from the table', () => {
-      expect(row[0]).toBe(table[i])
-    })
-
-    test('pushes a copy of the parent scope onto the stack before all', () => {
-      expect(groupContext).toMatchObject(groupContextBefore)
-    })
-
-    test('merges loop context into group context before all', () => {
-      expect(groupContext).toMatchObject(loopContext)
-    })
-
-    test('assigns group context to test context before each', () => {
-      expect(testContext).toBe(groupContext)
-    })
-
-    afterAll(() => { i++ })
-  })
-
-  test('calls the hook for all table rows', () => {
-    expect(i).toEqual(table.length)
-  })
-
-  test('pops the scope off the stack after all', (context) => {
-    expect(context).toBe(groupContextBefore)
-  })
-})
-
-describe('given a table of values', () => {
-
-  const table = [
-    "a",
-    "b",
-    "c"
-  ]
-
-  let i, groupContextBefore
-
-  beforeAll((context) => {
-    i = 0
-    groupContextBefore = context
-  })
-
-  describe.each(table)('iteration %#', (...row) => {
-
-    let groupContext, loopContext, testContext
-
-    beforeAll((context) => {
-      groupContext = context
-      loopContext = {
-        '0': row[0]
+    test('copies parent group context to group context before all', () => {
+      const parentGroupContext = outerBeforeAllFn.mock.results[0].value
+      for (let i = 0, expected; i < table.length; i++) {
+        expected = expect.objectContaining(parentGroupContext)
+        expect(innerBeforeAllFn).toHaveBeenNthCalledWith(i+1, expected)
       }
-    })
-
-    beforeEach((context) => {
-      testContext = context
-    })
-
-    test('calls the hook with a row from the table', () => {
-      expect(row[0]).toBe(table[i])
-    })
-
-    test('pushes a copy of the parent scope onto the stack before all', () => {
-      expect(groupContext).toMatchObject(groupContextBefore)
+      expect(innerBeforeAllFn).toHaveBeenCalledTimes(table.length)
     })
 
     test('merges loop context into group context before all', () => {
-      expect(groupContext).toMatchObject(loopContext)
+      for (let i = 0, loopContext, expected; i < table.length; i++) {
+        loopContext = contextFromLoopArgs(table[i])
+        expected = expect.objectContaining(loopContext)
+        expect(innerBeforeAllFn).toHaveBeenNthCalledWith(i+1, expected)
+      }
+      expect(innerBeforeAllFn).toHaveBeenCalledTimes(table.length)
     })
-
-    test('assigns group context to test context before each', () => {
-      expect(testContext).toBe(groupContext)
-    })
-
-    afterAll(() => { i++ })
   })
-
-  test('calls the hook for all table rows', () => {
-    expect(i).toEqual(table.length)
-  })
-
-  test('pops the scope off the stack after all', (context) => {
-    expect(context).toBe(groupContextBefore)
-  })
-})
+}
--- spec/describe.spec.js
+++ spec/describe.spec.js
@@ -1,42 +1,61 @@
-import { expect } from '@jest/globals'
-import { afterAll, beforeAll, beforeEach, describe, test } from '../src/index.js'
-import { generate } from 'randomstring'
+import { expect, jest } from '@jest/globals'
+import { afterAll, afterEach, beforeAll, beforeEach, describe, test } from '../src/index.js'
+import { fakeContext } from './helpers.js'
 
-function fakeContext(name) {
-  return { [generate()]: name }
-}
+const outerBeforeAllFn = jest.fn(() => fakeContext())
+const outerBeforeEachFn = jest.fn(() => fakeContext())
+const innerBeforeAllFn = jest.fn(() => fakeContext())
+const innerBeforeEachFn = jest.fn(() => fakeContext())
+const innerTestFn = jest.fn()
+const innerAfterAllFn = jest.fn()
+const innerAfterEachFn = jest.fn()
 
-const outerGroupContext = fakeContext('outer group')
-const innerGroupContext = fakeContext('inner group')
-const testContext = fakeContext('test context')
+// Warning: Do not change the following sequence:
 
-beforeAll((prev) => ({ ...prev, outerGroupContext }))
-beforeEach((prev) => ({ ...prev, innerGroupContext }))
+beforeAll(outerBeforeAllFn)
+beforeEach(outerBeforeEachFn)
 
-describe('describe', () => {
-  const innerScope = {}
-
-  beforeAll((context) => {
-    expect(context).toBe(context)
-    return { ...context, innerGroupContext }
-  })
-
-  beforeEach((context) => {
-    expect(context).toBe(testContext)
-    return { ...context, testContext }
-  })
-
-  test('pushes a new scope onto the stack before all', () => {
-    expect(innerScope.groupContext).toEqual(outerScope.groupContext)
-    expect(innerScope.groupContext).not.toBe(outerScope.groupContext)
-  })
-
-  test('assigns group context to test context before each', () => {
-    expect(innerScope.testContext).toBe(outerScope.testContext)
-  })
+describe('mock describe', () => {
+  beforeAll(innerBeforeAllFn)
+  beforeEach(innerBeforeEachFn)
+  test('mock test', innerTestFn)
+  afterAll(innerAfterAllFn)
+  afterEach(innerAfterEachFn)
 })
 
-test('pops the scope off the stack after all', (context) => {
-  expect(context).toBe(outerScope.testContext)
-  expect(outerScope.testContext).toBe(outerScope.groupContext)
+test('copies parent group context to group context before all', () => {
+  const parentGroupContext = outerBeforeAllFn.mock.results[0].value
+  expect(innerBeforeAllFn).toHaveBeenCalledWith(parentGroupContext)
+})
+
+test('assigns parent test context to test context before each', () => {
+  const parentTestContext = outerBeforeEachFn.mock.results[0].value
+  expect(innerBeforeEachFn).toHaveBeenCalledWith(parentTestContext)
+})
+
+test('provides group context to group-scope hooks', () => {
+  const innerGroupContext = innerBeforeAllFn.mock.results[0].value
+  expect(innerAfterAllFn).toHaveBeenCalledWith(innerGroupContext)
+})
+
+test('provides test context to test-scope hooks', () => {
+  const innerTestContext = innerBeforeEachFn.mock.results[0].value
+  expect(innerTestFn).toHaveBeenCalledWith(innerTestContext)
+  expect(innerAfterEachFn).toHaveBeenCalledWith(innerTestContext)
+})
+
+test('does not leak group context', (context) => {
+  const outerGroupContext = outerBeforeAllFn.mock.results[0].value
+  const innerGroupContext = innerBeforeAllFn.mock.results[0].value
+  expect(context).not.toMatchObject(innerGroupContext)
+  expect(outerBeforeEachFn.mock.calls.length).toBeGreaterThan(1)
+  expect(outerBeforeEachFn).toHaveBeenNthCalledWith(1, innerGroupContext)
+  for (let n = 2; n <= outerBeforeEachFn.mock.calls.length; n++) {
+    expect(outerBeforeEachFn).toHaveBeenNthCalledWith(n, outerGroupContext)
+  }
+})
+
+test('does not leak test context', (context) => {
+  const innerTestContext = innerBeforeAllFn.mock.results[0].value
+  expect(context).not.toMatchObject(innerTestContext)
 })
