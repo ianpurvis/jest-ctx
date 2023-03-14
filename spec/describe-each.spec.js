ab8c5c557b6eae6f5aa6d6ed504ae4dec0830be8
Ian Purvis <ian@purvisresearch.com>
2023-03-14 15:11:24 -0500

    Extract table examples to shared fixtures

--- spec/describe-each.spec.js
+++ spec/describe-each.spec.js
@@ -1,36 +1,10 @@
 import { expect, jest } from '@jest/globals'
 import { beforeAll, describe, test } from '../src/index.js'
+import { loopSubjects } from './fixtures.js'
 import { randomString } from './helpers.js'
 
-const subjects = [
-  {
-    name: 'a table of arrays',
-    table:[
-      [ 0, 0, 0 ],
-      [ 0, 1, 1 ],
-      [ 1, 0, 1 ]
-    ]
-  },
-  {
-    name: 'a table of objects',
-    table: [
-      { a: 0, b: 0, c: 0 },
-      { a: 0, b: 1, c: 1 },
-      { a: 1, b: 0, c: 1 }
-    ]
-  },
-  {
-    name: 'a table of values',
-    table: [
-      1,
-      2,
-      3
-    ]
-  }
-]
-
-for (const { name, table } of subjects) {
-  describe(`given ${name}`, () => {
+for (const { description, table } of loopSubjects) {
+  describe(`given ${description}`, () => {
     const beforeAllFn = jest.fn(() => randomString())
     const testFn = jest.fn()
     const describeFn = jest.fn(() => {
---
+++ spec/fixtures.js
@@ -0,0 +1,26 @@
+export const loopSubjects = [
+  {
+    description: 'a table of arrays',
+    table: [
+      [ 0, 0, 0 ],
+      [ 0, 1, 1 ],
+      [ 1, 0, 1 ]
+    ]
+  },
+  {
+    description: 'a table of objects',
+    table: [
+      { a: 0, b: 0, c: 0 },
+      { a: 0, b: 1, c: 1 },
+      { a: 1, b: 0, c: 1 }
+    ]
+  },
+  {
+    description: 'a table of values',
+    table: [
+      1,
+      2,
+      3
+    ]
+  }
+]
