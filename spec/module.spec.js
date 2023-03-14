7e3637b63e38bccc8ff894f55d6b07ce6dfeea82
Ian Purvis <ian@purvisresearch.com>
2023-03-14 17:24:12 -0500

    Add module spec

---
+++ spec/module.spec.js
@@ -0,0 +1,17 @@
+import { expect, test } from '@jest/globals'
+import * as native from '@jest/globals'
+import * as module from '../src/index.js'
+
+const keys = (o) => Object.keys(o).sort()
+
+test('it includes all globals', () => {
+  expect(keys(module)).toEqual(keys(native))
+})
+
+test('it includes all test extensions', () => {
+  expect(keys(module.test)).toEqual(keys(native.test))
+})
+
+test('it includes all describe extensions', () => {
+  expect(keys(module.describe)).toEqual(keys(native.describe))
+})
