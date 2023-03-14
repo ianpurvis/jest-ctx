7be34a650bef48eb06a9843c290f448c9fd0b853
Ian Purvis <ian@purvisresearch.com>
2023-03-14 14:29:22 -0500

    Remove obsolete contextFromLoopArgs utility

--- src/util.js
+++
@@ -1,9 +0,0 @@
-export function contextFromLoopArgs(...args) {
-  let context
-  if (args.length === 1 && args[0] === Object(args[0])) {
-    context = args[0]
-  } else {
-    context = args.reduce((prev, next, i) => ({ ...prev, [i]: next }), {})
-  }
-  return context
-}
