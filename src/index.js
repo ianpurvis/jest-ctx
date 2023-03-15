2f318f3b2c148fcbfcefdbea9a43d0c137d45f49
Ian Purvis <ian@purvisresearch.com>
2023-03-15 11:01:20 -0500

    Implement missing hooks

--- src/index.js
+++ src/index.js
@@ -42,13 +42,26 @@ export function beforeEach(fn, timeout) {
 export const describe = adaptDescribeHook(native.describe)
 describe.each = (table) => adaptDescribeHook(native.describe.each(table))
 describe.only = adaptDescribeHook(native.describe.only)
+describe.only.each = (table) => adaptDescribeHook(native.describe.only.each(table))
 describe.skip = native.describe.skip
 
 export const test = adaptTestHook(native.test)
 test.concurrent = adaptTestHook(native.test.concurrent)
+test.concurrent.each = (table) => adaptTestHook(native.test.concurrent.each(table))
+test.concurrent.failing = adaptTestHook(native.test.concurrent.failing)
+test.concurrent.failing.each = (table) => adaptTestHook(native.test.concurrent.failing.each(table))
+test.concurrent.only = adaptTestHook(native.test.concurrent.only)
+test.concurrent.only.each = (table) => adaptTestHook(native.test.concurrent.only.each(table))
+test.concurrent.only.failing = adaptTestHook(native.test.concurrent.only.failing)
+test.concurrent.only.failing.each = (table) => adaptTestHook(native.test.concurrent.only.failing.each(table))
+test.concurrent.skip = native.test.concurrent.skip
 test.each = (table) => adaptTestHook(native.test.each(table))
 test.failing = adaptTestHook(native.test.failing)
+test.failing.each = (table) => adaptTestHook(native.test.failing.each(table))
 test.only = adaptTestHook(native.test.only)
+test.only.each = (table) => adaptTestHook(native.test.only.each(table))
+test.only.failing = adaptTestHook(native.test.only.failing)
+test.only.failing.each = (table) => adaptTestHook(native.test.only.failing.each(table))
 test.skip = native.test.skip
 test.todo = native.test.todo
 
