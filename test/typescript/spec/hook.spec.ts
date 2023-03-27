835966abd41a9729e762b7d83d43c1e7be55e1eb
Ian Purvis <ian@purvisresearch.com>
2023-03-27 12:43:58 -0500

    Add typescript compilation tests

---
+++ test/typescript/spec/hook.spec.ts
@@ -0,0 +1,26 @@
+import type { Global as native } from '@jest/types'
+import type { CtxHookFn } from 'jest-ctx'
+import { afterAll, afterEach, beforeAll, beforeEach, } from 'jest-ctx'
+
+let fn: CtxHookFn
+
+// it works with ctx
+fn = (ctx) => ctx + 1
+
+// it works without ctx
+fn = () => {}
+
+// it works with async
+fn = async (ctx) => ctx + 1
+
+// it is compatible with native HookFn:
+const nativeFn: native.HookFn = fn
+fn = nativeFn
+
+// it is compatible with all hooks:
+let timeout: number | undefined
+
+beforeAll(fn, timeout)
+beforeEach(fn, timeout)
+afterEach(fn, timeout)
+afterAll(fn, timeout)
---
+++ test/typescript/spec/test.spec.ts
@@ -0,0 +1,45 @@
+import type { Global as native } from '@jest/types'
+import type { CtxTestFn } from 'jest-ctx'
+import { test } from 'jest-ctx'
+
+let fn: CtxTestFn
+
+// it works with ctx
+fn = (ctx) => { ctx + 1 }
+
+fn = (ctx: string) => {}
+
+// it works without ctx
+fn = () => {}
+
+// it works with async
+fn = async (ctx) => { ctx + 1 }
+
+// it is compatible with native TestFn:
+const nativeFn: native.TestFn = fn
+fn = nativeFn
+
+// it is compatible with all test variants
+let name: native.TestNameLike
+let timeout: number | undefined
+let table: []
+
+test(name, fn, timeout)
+test.concurrent(name, fn, timeout)
+test.concurrent.each(table)(name, fn, timeout)
+test.concurrent.failing(name, fn, timeout)
+test.concurrent.failing.each(table)(name, fn, timeout)
+test.concurrent.only(name, fn, timeout)
+test.concurrent.only.each(table)(name, fn, timeout)
+test.concurrent.only.failing(name, fn, timeout)
+test.concurrent.only.failing.each(table)(name, fn, timeout)
+test.concurrent.skip(name, fn, timeout)
+test.each(table)(name, fn, timeout)
+test.failing(name, fn, timeout)
+test.failing.each(table)(name, fn, timeout)
+test.only(name, fn, timeout)
+test.only.each(table)(name, fn, timeout)
+test.only.failing(name, fn, timeout)
+test.only.failing.each(table)(name, fn, timeout)
+test.skip(name, fn, timeout)
+test.todo(name)
